c7e876f16198d47d2ac384519268b0806a387fe4
Miles Higbie <miles.higbie@gmail.com>
2020-05-25 21:28:05 -0400

    added nodemon for live reload

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (request,response) => {
     response.send('Hello World!')
 })
 
+app.get('/api/courses', (request,response) => {
+    response.send([1,2,3])
+})
+
 app.listen(3000, () => {
     console.log("Listening on port 3000")
 })
