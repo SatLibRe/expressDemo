18c0d628816966f1d0359f72a4db9f44afe51def
Miles Higbie <miles.higbie@gmail.com>
2020-05-26 11:04:23 -0400

    sample post added joi for error handleing

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express();
+app.use(express.json())
+const port = process.env.PORT || 3000
+
+const courses = [
+    {id: 1, name: 'Math'},
+    {id: 2, name: 'English'},
+    {id: 3, name: 'Science'}
+]
+
+///Server
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
+})
+
+
+///Get requests 
 
 app.get('/', (request,response) => {
     response.send('Hello World!')
 })
 
 app.get('/api/courses', (request,response) => {
-    response.send([1,2,3])
+    response.send(courses)
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+app.get('/api/courses/:id', (request, response) => {
+    const course = courses.find(course => course.id == request.params.id )
+    if (!course) response.status(404).send("The course with the given ID was not found")
+    response.send(course)
+})
+
+//Post Requests 
+
+app.post('/api/courses', (request,response) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(request.body, schema)
+    console.log(result)
+    
+   if(result.error){
+       response.status(400).send(result.error.details[0].message)
+   }
+    
+    const course = {
+        id: courses.length + 1,
+        name: request.body.name
+    }
+    courses.push(course);
+    response.send(course)
 })
